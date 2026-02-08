03b6928de112f912c8079e2db166bb1e05a97072
Ayan Mondal <ayanmondal0914@gmail.com>
2026-02-08 23:35:55 +0530

    link tag addad on navbar and some changes in footer and home.jsx file

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -4,7 +4,10 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <div className="sticky top-0 z-50 bg-linear-to-r from-[#360b68] to-[#0e8dc8] text-white flex justify-between py-4 px-6 h-16">
-     <div className="flex items-center gap-3"><img className="h-7.5 w-7.5 rounded-full" src="https://fusemarket.in/assets/images/logo-bg.jpg" alt="" /><h1 className="font-bold text-xl">FusseMarket Pvt. Ltd.</h1></div>
+     <a href="https://fusemarket.in/" target="_blank" rel="noopener noreferrer" className="flex items-center gap-3">
+       <img className="h-7.5 w-7.5 rounded-full" src="https://fusemarket.in/assets/images/logo-bg.jpg" alt="FusseMarket logo" />
+       <h1 className="font-bold text-xl">FusseMarket Pvt. Ltd.</h1>
+     </a>
       <div className="space-x-6">
         <ul className="flex space-x-6">     
             <li><Link to="/">Home</Link></li>
